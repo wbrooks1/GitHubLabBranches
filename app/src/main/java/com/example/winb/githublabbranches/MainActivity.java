bae78a16321578d6767d2546e6e10949620f9bf2
POSTED26 <Tilbo2@gmail.com>
2016-10-12 15:30:11 -0700

    first commit

--- app/src/main/java/com/example/winb/githublabbranches/MainActivity.java
+++ app/src/main/java/com/example/winb/githublabbranches/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.winb.githublabbranches;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendJacob(View view) {
+        Intent intent = new Intent(this, JacobActivity.class);
+        startActivity(intent);
     }
 }
